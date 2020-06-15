e71d4e87b2a9c0172bf2ff0fd5316113ef52299a
Killian O'Neachtain <killianoneachtain@hotmail.com>
2020-06-15 14:57:30 +0100

    Movie Details Page rendering

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
+import MoviePage from './pages/movieDetailsPage'
 
 const sample = {
   adult: false,
@@ -84,12 +85,12 @@ const sample = {
   vote_count: 9692
 };
 
-const movies = [sample, sample, sample];
+//const movies = [sample, sample, sample];
 
 const App = () => {
   return (
     <div className="jumbotron">
-      <HomePage movies={movies} />
+      <MoviePage movie={sample} />
     </div>
   );
 };
