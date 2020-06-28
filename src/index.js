cc1c0cafcce32084fa5a50df3ddc05b8b8375274
Killian O'Neachtain <killianoneachtain@hotmail.com>
2020-06-28 14:42:48 +0100

    Change site header

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link} from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/homePage";
 import MoviePage from './pages/movieDetailsPage'
-import FavoriteMoviesPage from './pages/favoritesMoviesPage'  // NEW Week 3
+import FavoriteMoviesPage from './pages/favoritesMoviesPage';  // NEW Week 3
 import MovieReviewPage from "./pages/movieReviewPage"; // New Week 4
+import SiteHeader from './components/siteHeader'; // NEW Week 4 
 
 const App = () => {
   return (
     <BrowserRouter>
     <div className="jumbotron">
-      <div className="container-fluid">
-        <ul className="navbar-nav text-black">
-          <li className="nav-item">
-            <Link className="nav-link " to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link " to="/movies/favorites">
-              Favorites
-            </Link>
-          </li>
-        </ul>
-        <Switch>
+    <SiteHeader />      {/* New Header  */}
+          <div className="container-fluid">
+            <Switch>
           <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
