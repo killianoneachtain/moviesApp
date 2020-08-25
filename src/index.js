c246a180c485fcc59dfea59abecd313b933f4bf7
Killian O'Neachtain <48589439+killianoneachtain@users.noreply.github.com>
2020-08-25 20:48:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const App = () => {
     <BrowserRouter>
     <Auth0Provider
         domain="dev-qb0ckde9.us.auth0.com"
-        clientId="swmPsHx297SnEltxsW3Nuiw0ZEa4g0BK"
+        clientId="QxzS75tUvwiAS7NRG313JQYHNMnZnkcL"
         redirectUri={window.location.origin}>
       <div className="jumbotron">
         <SiteHeader />      
